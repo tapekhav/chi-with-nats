c4edf7a231dcbba7bae23f97727b5bbe491f3034
tapehavladimir@gmail.com <tapehavladimir@gmail.com>
2024-02-08 22:58:56 +0300

    feat: add docker

--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require github.com/nats-io/nats.go v1.32.0
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
@@ -19,4 +21,5 @@ require (
 	github.com/nats-io/nuid v1.0.1 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	google.golang.org/appengine v1.6.8
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,24 +1,33 @@
 package config
 
 import (
-	"fmt"
+	"context"
+	"os"
 
 	"github.com/ilyakaznacheev/cleanenv"
+	"google.golang.org/appengine/log"
 )
 
 type Config struct {
-	SubjectName   string `env:"SUBJECT_NAME" env-default:"example"`
 	NatsAddr  	  string `env:"NATS_ADDR" env-default:":4222"`
+	SubjectName   string `env:"SUBJECT_NAME" env-default:"example"`
 	PublisherAddr string `env:"PUBLISHER_ADDR" env-default:":8080"`
 }
 
 func MustLoad() Config {
-	var cfg Config
+	path := os.Getenv("CFG_PATH")
 
-	err := cleanenv.ReadConfig("../../config.env", &cfg)
+	var err error
+	var cfg Config
+	if path != "" {
+		err = cleanenv.ReadConfig(path, &cfg)
+	} else {
+		err = cleanenv.ReadEnv(&cfg)
+	}
 
 	if err != nil {
-		panic(fmt.Sprintf("Cannot read config: %s", err))
+		ctx := context.Background()
+		log.Warningf(ctx, "Cannot read config: %s", err)
 	}
 
 	return cfg
